c15349a55f499777fad4a802086906e3e5e17fa0
Harry Atkinson <123966575+HarryAtkin@users.noreply.github.com>
2025-03-01 21:07:20 +0000

    Finished the second bonus task.

--- AireLogic.py
+++ AireLogic.py
@@ -9,13 +9,26 @@ class AirOxygenEnum(Enum):
 #Creates enum for if the patient is conscious.
 class ConsciousnessEnum(Enum):
     ALERT = 0
-    CVPU = 1  #Have a look at making this work for any non-zero int.
+    CVPU = 1
+
+#Creates enum for if the patient is fasting or not.
+class FastingEnum(Enum):
+    FASTING = 0
+    NOTFASTING = 1
 
 
 class PatientDetails:
 
     #Constructor initialises the attributes
-    def __init__(self, patient_id : int, air_oxygen: int, consciousness: int, respiration_rate: int, spo2: int, temperature: float):
+    def __init__(self, patient_id : int, air_oxygen: int, consciousness: int, respiration_rate: int, spo2: int, temperature: float, fasting : int, cbg : float):
+
+        # these attributes types are validated when passed into the function.
+        self.PatientId = patient_id
+        self.RespirationRate = respiration_rate
+        self.SpO2 = spo2
+        # Rounds the below to 1 dp.
+        self.Temperature = round(temperature, 1)
+        self.CBG = round(cbg, 1)
 
         #Validation
         #Checks that the input is one of the values in the AirOxygenEnum.
@@ -25,8 +38,8 @@ class PatientDetails:
         else:
             #Runs until a valid input is entered.
             while air_oxygen != 0 and air_oxygen != 2:
-                air_oxygen = int(input("Invalid input for Air or Oxygen\n"
-                                           "please enter 0 for Air or 2 for Oxygen\n"))
+                air_oxygen = int(input(f'Patient {self.PatientId} has Invalid input for Air or Oxygen\n'
+                                           f'please enter 0 for Air or 2 for Oxygen\n'))
                 self.AirOxygen = air_oxygen
 
 
@@ -37,12 +50,18 @@ class PatientDetails:
             #Sets their consciousness to the value of CVPU from the ConsciousnessEnum.
             self.Consciousness = ConsciousnessEnum.CVPU.value
 
+        #Checks if the fasting input is valid or not.
+        if fasting == FastingEnum.FASTING.value or fasting == FastingEnum.NOTFASTING.value:
+            self.Fasting = fasting
 
-        #these attributes types are validated when passed into the function.
-        self.PatientId = patient_id
-        self.RespirationRate = respiration_rate
-        self.SpO2 = spo2
-        self.Temperature = round(temperature, 1)
+        else:
+            #Runs until a valid input is entered.
+            while fasting != 0 and fasting != 1:
+                fasting = int(input(f'Patient {self.PatientId} has Invalid input for if the patient is fasting or not\n'
+                                           f'please enter 0 for Fasting or 1 for not Fasting\n'))
+                self.Fasting = fasting
+
+
 
         #Sets this variable to none so it can be set later.
         self.MediScore = None
@@ -74,12 +93,16 @@ class PatientDetails:
     #To string method (human-readable class when printed).
     def __str__(self):
         return (f'PatientId: {self.PatientId}\n'
+                f'Time tested: {self.MediScoreTime}\n'
                 f'Air or Oxygen: {AirOxygenEnum(self.AirOxygen).name}\n'
                 f'Consciousness: {ConsciousnessEnum(self.Consciousness).name}\n'
                 f'Respiration Rate: {self.RespirationRate} per minute\n'
                 f'SpO2: {self.SpO2}%\n'
                 f'Temperature: {self.Temperature}°C\n'
+                f'CBG: {self.CBG}mmol/L : {FastingEnum(self.Fasting).name}\n'
                 f'MediScore: {self.MediScore}\n'
+                
+                
                 f'-------------------------------------------------------')
 
 
@@ -139,13 +162,41 @@ def mediscore(patient: PatientDetails):
         else:
             FinalScore += 2
 
+    #This checks for if the Patient is fasting and calculates their score based on that.
+    if patient.Fasting == FastingEnum.FASTING.value:
+        if patient.CBG < 4:
+            if patient.CBG >= 3.5:
+                FinalScore += 2
+            else:
+                FinalScore += 3
+
+        elif patient.CBG > 5.4:
+            if patient.CBG <= 5.9:
+                FinalScore += 2
+            else:
+                FinalScore += 3
+
+    #Runs when the patient is not fasting.
+    else:
+        if patient.CBG < 5.9:
+            if patient.CBG > 4.5:
+                FinalScore += 2
+            else:
+                FinalScore += 3
+
+        elif patient.CBG > 7.8:
+            if patient.CBG <= 8.9:
+                FinalScore += 2
+            else:
+                FinalScore += 3
+
     return FinalScore
 
 
 print("Test Case 1\n============")
 #====================================================================================================================================#
 #Creates patient 1, calculates MediScore.
-Patient1 = PatientDetails(1,0, 0, 15, 95, 37.1)
+Patient1 = PatientDetails(1,0, 0, 15, 95, 37.19, 1, 5.98)
 
 #This creates 24 tests of patient 1. 1 For each hour of the day to test the alert system for the increase of Mediscore.
 #This test will cause the alert to go off.
@@ -161,7 +212,7 @@ print(Patient1)
 print("Test Case 2\n============")
 #====================================================================================================================================#
 #Creates patient 2, calculates MediScore.
-Patient2 = PatientDetails(2, 2, 0, 17, 95, 37.1)
+Patient2 = PatientDetails(2, 2, 0, 17, 95, 37.1, 0, 5)
 
 #This creates 12 tests of patient 1. 1 For every other hour of the day to test the alert system for the increase of Mediscore.
 #This test will not cause the alert to go off as the MediScore only increased by 1.
@@ -170,7 +221,7 @@ for x in range (12):
 print(Patient2)
 
 Patient2.RespirationRate = 11
-Patient2.SetMediScore(mediscore(Patient2), datetime.datetime.now())
+Patient2.SetMediScore(mediscore(Patient2), datetime.datetime.strptime("2025-03-01 00:01:10", "%Y-%m-%d %H:%M:%S"))
 print(Patient2)
 #====================================================================================================================================#
 print("Test Case 3\n============")
@@ -182,9 +233,10 @@ print("Test Case 3\n============")
 #Respiration Rate = 23 (+2)
 #SpO2 % = 88 (+0)
 #Temperature °C = 38.5 (+1)
-# 2+3+2+1 = 8 which == the output.
+#CBG (Fasting) = 5 (+2)
+# 2+3+2+1+2 = 10 which == the output.
 
-Patient3 = PatientDetails(3, 2, 1, 23, 88, 38.5)
+Patient3 = PatientDetails(3, 2, 1, 23, 88, 38.5, 1, 5)
 Patient3.SetMediScore(mediscore(Patient3), datetime.datetime.now())
 print(Patient3)
 #====================================================================================================================================#
@@ -193,7 +245,7 @@ print("Test Case 4\n============")
 #Creates patient 4, then prints the patient.
 #Patient 4 contains an error to test the validation on the air or oxygen field with an input of 3.
 #It also tests that any number that isn't 0 means CVPU
-Patient4 = PatientDetails(4, 3, 6, 22, 95, 38.5)
+Patient4 = PatientDetails(4, 3, 6, 22, 95, 38.5, 3, 6)
 Patient4.SetMediScore(mediscore(Patient4), datetime.datetime.now())
 print(Patient4)
 
